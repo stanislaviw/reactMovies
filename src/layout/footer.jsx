212ff3719f5e68fa3145a47d48e56219a40c4a94
stanislaviw <slavi8ok@gmail.com>
2022-11-19 21:41:57 +0200

    bagfix

--- src/layout/footer.jsx
+++ src/layout/footer.jsx
@@ -3,8 +3,8 @@ function Footer () {
         <footer className="page-footer light-blue darken-4">
           <div className="footer-copyright">
             <div className="container">
-              © {new Date().getFullYear()} Copyright Text
-              <a className="grey-text text-lighten-4 right" href="#!">Repo</a>
+              <span>© {new Date().getFullYear()} Copyright Text</span>
+              <a className="grey-text text-lighten-4 right" href="https://github.com/stanislaviw/reactMovies">Repositorie</a>
             </div>
           </div>
         </footer>
--- src/layout/header.jsx
+++ src/layout/header.jsx
@@ -3,10 +3,10 @@ function Header () {
     return (
         <nav className="light-blue darken-4">
             <div className="nav-wrapper">
-            <a href="!#" className="brand-logo">React Movies</a>
-            <ul id="nav-mobile" className="right hide-on-med-and-down">
-                <li><a href="!#">Repo</a></li>
-            </ul>
+                <a href="#" className="brand-logo">React Movies</a>
+                <ul id="nav-mobile" className="right hide-on-med-and-down">
+                    <li><a href="https://github.com/stanislaviw/reactMovies">Repositorie</a></li>
+                </ul>
             </div>
         </nav>
     )
